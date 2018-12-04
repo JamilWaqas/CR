8418be8feb96e4b48ca36aabb9d4ef9c5acdd2f1
Waqas Jamil <dr.wjamil@gmail.com>
2018-12-04 15:06:00 +0000

    Update demo.R

--- demo.R
+++ demo.R
@@ -30,12 +30,17 @@ Xtemp<-cbind(X,pw,sp,hum,hum2)
 Ytemp<- as.matrix(as.numeric(data1[,4]))
 
 
-print(benchmarks(Xtemp,Ytemp))
-print(AAR(Xtemp,Ytemp,1))
-print(ORR(Xtemp,Ytemp,1))
-print(OSLOG(Xtemp,Ytemp,1))
-print(COIRR(Xtemp,Ytemp,1))
+print(benchmarks(Xtemp,Ytemp)$performance)
+print(benchmarks(Xtemp,Ytemp)$quantiles)
 
+print(AAR(Xtemp,Ytemp,1)$performance)
+print(ORR(Xtemp,Ytemp,1)$quantiles)
+
+print(OSLOG(Xtemp,Ytemp,1)$performance)
+print(OSLOG(Xtemp,Ytemp,1)$quantiles)
+
+print(COIRR(Xtemp,Ytemp,1)$performance)
+print(COIRR(Xtemp,Ytemp,1)$quantiles)
 
 #NO2 Data
 data<- NO2
@@ -46,19 +51,37 @@ X<-data.frame(X)
 XNO2<-as.matrix(cbind(rep(1,500),X))
 YNO2<- as.matrix(data[,1])
 
-print(benchmarks(XNO2,YNO2))
-print(AAR(XNO2,YNO2,1))
-print(ORR(XNO2,YNO2,1))
-print(OSLOG(XNO2,YNO2,1))
-print(COIRR(XNO2,YNO2,1))
+print(benchmarks(XNO2,YNO2)$performance)
+print(benchmarks(XNO2,YNO2)$quantiles)
 
+print(AAR(XNO2,YNO2,1)$performance)
+print(AAR(XNO2,YNO2,1)$quantiles)
+
+print(ORR(XNO2,YNO2,1)$performance)
+print(ORR(XNO2,YNO2,1)$quantiles)
+
+print(OSLOG(XNO2,YNO2,1)$performance)
+print(OSLOG(XNO2,YNO2,1)$quantiles)
+
+print(COIRR(XNO2,YNO2,1)$performance)
+print(COIRR(XNO2,YNO2,1)$quantiles)
 #ISE
 data<- ISE
 XISE<- as.matrix(data[,-c(1,3)])
 YISE<- as.matrix(data[,3])
 
-print(benchmarks(XISE,YISE))
-print(AAR(XISE,YISE,0.001953125))
-print(ORR(XISE,YISE,1.525879e-05))
-print(OSLOG(XISE,YISE,0.0001220703))
-print(COIRR(XISE,YISE,0.0009765625))
+print(benchmarks(XISE,YISE)$performance)
+print(benchmarks(XISE,YISE)$quantiles)
+
+print(AAR(XISE,YISE,0.001953125)$performance)
+print(AAR(XISE,YISE,0.001953125)$quantiles)
+
+print(ORR(XISE,YISE,0.0001220703)$performance)
+print(ORR(XISE,YISE,0.0001220703)$quantiles)
+
+print(COIRR(XISE,YISE,0.0009765625)$performance)
+print(COIRR(XISE,YISE,0.0009765625)$quantiles)
+
+print(OSLOG(XISE,YISE,1.525879e-05)$performance)
+print(OSLOG(XISE,YISE,1.525879e-05)$quantiles)
+
