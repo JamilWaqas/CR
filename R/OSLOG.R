7600380b72f726072c10f5bd37b46ab90db39aa1
Waqas Jamil <dr.wjamil@gmail.com>
2018-12-21 05:12:00 +0000

    Update OSLOG.R

--- R/OSLOG.R
+++ R/OSLOG.R
@@ -8,6 +8,7 @@
 #' Y<-rnorm(10)
 #' OSLOG(X,Y,a=1)
 
+
 OSLOG<-function(X,Y,a){
   if(a<=0){
     print("a must be a positive number")
@@ -18,23 +19,21 @@ OSLOG<-function(X,Y,a){
     N<-ncol(X)
     bt<- matrix(0,ncol=1,nrow=N)
     At<- diag(0,N)
-    AAt<- diag(N)*1/N
     pred<- matrix(0,nrow=T,ncol=1)
     theta0<- rep(1,N)
     for (t in 1:T){
-      pred[t] <- t(as.matrix(theta0)) %*% X[t,]
-      Dt <- diag(abs(as.numeric(theta0)))
-      At <- At + (X[t,] %*% t(X[t,]))
-      InvA <-  chol2inv(chol((a*diag(N) + (sqrt(Dt) %*% At %*% sqrt(Dt)))))
-      AAt<- sqrt(Dt) %*%InvA%*% sqrt(Dt)
+      pred[t] <- crossprod(as.matrix(theta0), X[t,])
+      Dt <- diag(sqrt(abs(c(theta0))))
+      At <- At + tcrossprod(X[t,],X[t,])
+      InvA <-  chol2inv(chol(a*diag(N) + outer(diag(Dt),diag(Dt)) * At))
+      AAt<- outer(diag(Dt),diag(Dt)) * InvA 
       bt <- bt + (Y[t] * X[t,])
-      theta0 <- AAt %*% bt
+      theta0 <- crossprod(AAt,bt) 
     }
     res<-postResample(pred = pred, obs = Y)
     stats<- as.matrix(res)[c(1,3),]
-    quant<-quantile(Y-pred,probs=c(.25,.50,.75))
-
+    quant<-quantile(as.matrix(Y)-as.matrix(pred),probs=c(.25,.50,.75))
+    
     return(list(predictions=pred,performance=stats,quantiles=quant))
   }
 }
-
