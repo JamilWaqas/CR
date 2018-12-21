134921ffabd96383230777de7a78ef4b928278f0
Waqas Jamil <dr.wjamil@gmail.com>
2018-12-21 05:20:08 +0000

    Update AAR.R

--- R/AAR.R
+++ R/AAR.R
@@ -13,21 +13,21 @@ AAR<- function(X,Y,a){
     print("a must be a positive number")
   }else{
     X<- as.matrix(X)
-    Y<- as.matrix(Y)
+    Y<-as.matrix(Y)
     N<- ncol(X)
     T<- nrow(X)
     bt<-matrix(0,ncol=1,nrow=N)
     At<-a * diag(1, N)
     pred<-matrix(0,ncol=1,nrow=T)
     for(t in 1:T){
-      At<- At + (X[t,] %*% t(X[t,]))
-      pred[t,]<-  t(bt)%*%chol2inv(chol((At)))%*%X[t,]
+      At<- At + tcrossprod(X[t,],X[t,])
+      pred[t,]<-  tcrossprod(X[t,],crossprod(bt,chol2inv(chol((At)))))
       bt<- bt + as.numeric(Y[t,]*X[t,])
     }
     res<-postResample(pred = pred, obs = Y)
     stats<- as.matrix(res)[c(1,3),]
     quant<-quantile(Y-pred,probs=c(.25,.50,.75))
-
+    
     return(list(predictions=pred,performance=stats,quantiles=quant))
   }
 }
