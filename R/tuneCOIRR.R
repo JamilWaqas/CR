e07b51e69f9d6507d89156c9a4a17c6eb62640e5
Waqas Jamil <dr.wjamil@gmail.com>
2018-12-28 16:44:59 +0000

    Update tuneCOIRR.R

--- R/tuneCOIRR.R
+++ R/tuneCOIRR.R
@@ -16,13 +16,14 @@
 #' Ytest<- as.matrix(test[,1])
 #' tuneCOIRR(0.1,0.2,0.01,Xtrain,Ytrain)
 
-tuneCOIRR<-function(from,to,by,Xtrain,Ytrain){
+tuneCOIRR<-function(from,to,by,Xtrain,Ytrain,b){
   a <- seq(from,to,by)
+  b <- seq(0,1,0.01)
   res<-matrix(0,ncol=1,nrow=length(a))
   pb <- txtProgressBar(min = 0, max = length(a), style = 3)
-  for(j in 1:length(a)) {
+  for(j in 1:length(a)){
     setTxtProgressBar(pb, j)
-    pre<- as.matrix(CR::COIRR(Xtrain,Ytrain,a[j])$predictions)
+    pre<- as.matrix(CR::COIRR(Xtrain,Ytrain,a[j],b)$predictions)
     res[j] <- sum((pre-Ytrain)^2)
   }
   close(pb)
