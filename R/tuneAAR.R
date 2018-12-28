8cc9299c6474f216a04769fdf6db1f852e47c7f2
Waqas Jamil <dr.wjamil@gmail.com>
2018-12-28 10:15:45 +0000

    Add files via upload

---
+++ R/tuneAAR.R
@@ -0,0 +1,32 @@
+#' tuneAAR
+#'
+#' @param This functions takes scalers; start value, end value, increment and input/output of the training set.
+#' @return vector of tuning parameter values, and optimum value.
+#' @export
+#' @examples
+#' X<-matrix(rexp(200, rate=.1), ncol=20)
+#' Y<-rnorm(10)
+#' data<- cbind(Y,X)
+#' sample <- 1:floor(.25*nrow(data))
+#' train <- data[sample, ]
+#' test  <- data[-sample, ]
+#' Xtrain<- as.matrix(train[,-1])
+#' Ytrain<-scale(as.matrix(train[,1]))
+#' Xtest<- as.matrix(test[,-c1])
+#' Ytest<- as.matrix(test[,1])
+#' tuneAAR(0.1,0.2,0.01,Xtrain,Ytrain)
+
+tuneAAR<-function(from,to,by,Xtrain,Ytrain){
+  a <- seq(from,to,by)
+  res<-matrix(0,ncol=1,nrow=length(a))
+  pb <- txtProgressBar(min = 0, max = length(a), style = 3)
+  for(j in 1:length(a)) {
+    setTxtProgressBar(pb, j)
+    pre<- as.matrix(CR::AAR(Xtrain,Ytrain,a[j])$predictions)
+    res[j] <- sum((pre-trainY)^2)
+  }
+  close(pb)
+  parm<-which.min(res)
+  opt<-a[parm]
+  return(list(result=res,optimum = opt))
+}
