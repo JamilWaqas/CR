2d6d2917ba3350a4cc9f44a526f2783ff300fe0e
Waqas Jamil <dr.wjamil@gmail.com>
2018-12-28 16:45:59 +0000

    Update tuneOSLOG.R

--- R/tuneOSLOG.R
+++ R/tuneOSLOG.R
@@ -16,13 +16,13 @@
 #' Ytest<- as.matrix(test[,1])
 #' tuneOSLOG(0.1,0.2,0.01,Xtrain,Ytrain)
 
-tuneOSLOG<-function(from,to,by,Xtrain,Ytrain){
+tuneOSLOG<-function(from,to,by,Xtrain,Ytrain,b){
   a <- seq(from,to,by)
   res<-matrix(0,ncol=1,nrow=length(a))
   pb <- txtProgressBar(min = 0, max = length(a), style = 3)
   for(j in 1:length(a)) {
     setTxtProgressBar(pb, j)
-    pre<- as.matrix(CR::OSLOG(Xtrain,Ytrain,a[j])$predictions)
+    pre<- as.matrix(CR::OSLOG(Xtrain,Ytrain,a[j],b)$predictions)
     res[j] <- sum((pre-Ytrain)^2)
   }
   close(pb)
