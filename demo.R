36c6d145363b10fcfb8aff73eefa8b4a6ddbc562
Waqas Jamil <dr.wjamil@gmail.com>
2018-12-29 13:12:20 +0000

    Update demo.R

--- demo.R
+++ demo.R
@@ -1,175 +1,95 @@
-# install.packages("devtools")
-# library(devtools)
-# install_github("JamilWaqas/CR")
-# install.packages(c("caret", "glmnet", "microbenchmark","MASS"))
+fried<-Fried
+sample <- 1:floor(.25*nrow(fried))
+train <- as.matrix(fried[sample,])
+test  <- as.matrix(fried[-sample,])
+Xtrain<- as.matrix(train[,1:10])
+Ytrain<-as.matrix(train[,11])
+Xtest<- as.matrix(test[,1:10])
+Ytest<- as.matrix(test[,11])
 
-# library(CR)
-# library(MASS)
-# library(glmnet)
-# library(caret)
-# library(microbenchmark)
+print(AAR(Xtrain,Ytrain,0.1)$Weights)
+print(ORR(Xtrain,Ytrain,0.5)$Weights)
+print(OSLOG(Xtrain,Ytrain,0.5)$Weights)
+print(COIRR(Xtrain,Ytrain,0.5)$Weights)
 
+print(AAR(Xtest,Ytest,0.1)$quantiles)
+print(ORR(Xtest,Ytest,0.5)$quantiles)
+print(OSLOG(Xtrain,Ytrain,0.5)$quantiles)
+print(COIRR(Xtest,Ytest,0.5)$quantiles)
 
-#Simulation
-set.seed(123)
-mu <- runif(100,0,1)
-Sigma <- matrix(.7, nrow=100, ncol=100) + diag(100)*.3
-x <- mvrnorm(n=10000, mu=mu, Sigma=Sigma)
-w<-c(runif(99,0,3),2.5)
-sigma <- 0.4
-eps <- rnorm(10000,0,1)
-for(i in 2:10000) eps[i] <- 0.15 * eps[i-1] + sqrt(1-0.15^2)*eps[i]
-eps <- sigma * eps
-y <-  x %*% w  + eps
-Y<- y
-X<- x
+print(AAR(Xtest,Ytest,0.1)$performance)
+print(ORR(Xtest,Ytest,0.5)$performance)
+print(OSLOG(Xtest,Ytest,0.5)$performance)
+print(COIRR(Xtest,Ytest,0.5)$performance)
 
-print(CR::benchmarks(X,Y)$performance)
-print(CR::AAR(X,Y,1)$performance)
-print(CR::ORR(X,Y,1)$performance)
-print(CR::COIRR(X,Y,1)$performance)
-print(CR::OSLOG(X,Y,1)$performance)
+print(OSLOG(Xtest[,-c(3,6,7,8,9,10)],Ytest,0.5)$performance)
+print(COIRR(Xtest[,-c(3,6,7,8,9,10)],Ytest,0.5)$performance)
 
+print(benchmarks(Xtest,Ytest)$performance)
+print(benchmarks(Xtest,Ytest)$quantiles)
 
-#Effect of range of Y
+Xtest[1000,3]<- 500
+Xtest[1000,1]<- 500
 
-mu <- runif(100,0,1)
-Sigma <- matrix(.7, nrow=100, ncol=100) + diag(100)*.3
-x <- mvrnorm(n=10000, mu=mu, Sigma=Sigma)
-w<-c(runif(99,0,3),2.5)
-sigma <- 0.4
-eps <- rnorm(10000,0,1)
-for(i in 2:10000) eps[i] <- 0.15 * eps[i-1] + sqrt(1-0.15^2)*eps[i]
-eps <- sigma * eps
-y <- scale(x %*% w)  + eps
-Y<- y
-X<-x
-
-print(CR::benchmarks(X,Y)$performance)
-print(CR::AAR(X,Y,1)$performance)
-print(CR::ORR(X,Y,1)$performance)
-print(CR::COIRR(X,Y,1)$performance)
-print(CR::OSLOG(X,Y,1)$performance)
+print(OSLOG(Xtest,Ytest,0.5)$performance)
+print(COIRR(Xtest,Ytest,0.5)$performance)
 
 
-#Effect of outlier
-
-X[5000,100]<-737377
-
-print(CR::benchmarks(X,Y)$performance)
-print(CR::AAR(X,Y,1)$performance)
-print(CR::ORR(X,Y,1)$performance)
-print(CR::COIRR(X,Y,1)$performance)
-print(CR::OSLOG(X,Y,1)$performance)
-
-
-#Effect of shrinkage
-
-set.seed(123)
-mu <- runif(100,0,1)
-Sigma <- matrix(.7, nrow=100, ncol=100) + diag(100)*.3
-x <- mvrnorm(n=10000, mu=mu, Sigma=Sigma)
-w<-c(runif(98,0,0.0),3.5,2.5)
-sigma <- 0.4
-eps <- rnorm(10000,0,1)
-for(i in 2:10000) eps[i] <- 0.15 * eps[i-1] + sqrt(1-0.15^2)*eps[i]
-eps <- sigma * eps
-y <-  x %*% w  + eps
-Y<- y
-X<-x
-
-print(CR::benchmarks(X,Y)$performance)
-print(CR::AAR(X,Y,1)$performance)
-print(CR::ORR(X,Y,1)$performance)
-print(CR::COIRR(X,Y,1)$performance)
-print(CR::OSLOG(X,Y,1)$performance)
-
-
-data("Temp", package="CR")
-data("NO2", package="CR")
-data("ISE", package="CR")
-
-data1<-Temp
-data<- as.matrix(data1[,-c(1,4)])
-X<- as.matrix(data[,-c(3)])
-x1<-as.numeric(as.factor(X[,1]))
-x2<-as.numeric(as.factor(X[,2]))
-x3<-as.numeric(as.factor(X[,9]))
-X1<-cbind(X[,3:8],x1,x2,x3)
-X <- mapply(X1, FUN=as.numeric)
-X<- matrix(data=X, nrow(X1),ncol(X1))
-X<-X[,-c(5)]
-pw<-X[,1]*X[,6]
-sp<-X[,1]^2
-hum<-exp(X[,1])
-hum2<-sin(X[,1])
-Xtemp<-cbind(X,pw,sp,hum,hum2)
-Ytemp<- as.matrix(as.numeric(data1[,4]))
-print(CR::benchmarks(Xtemp,Ytemp)$performance)
-print(CR::benchmarks(Xtemp,Ytemp)$quantiles)
-aar<-CR::AAR(Xtemp,Ytemp,1)
-orr<-CR::ORR(Xtemp,Ytemp,1)
-oslog<-CR::OSLOG(Xtemp,Ytemp,1)
-coirr<-CR::COIRR(Xtemp,Ytemp,1)
-
-perfTemp<-as.matrix(rbind(aar$performance,orr$performance,oslog$performance,coirr$performance))
-perfTemp<-cbind(perfTemp[,1],(perfTemp[,2]))
-colnames(perfTemp)<-c("RMSE","MAE")
-rownames(perfTemp)<-c("aar","orr","oslog","coirr")
-print(perfTemp)
-
-quantTemp<-as.matrix(rbind(aar$quantiles,orr$quantiles,oslog$quantiles,coirr$quantiles))
-colnames(quantTemp)<-c("25%","50%","75%")
-rownames(quantTemp)<-c("aar","orr","oslog","coirr")
-print(quantTemp)
-#NO2 Data
-data<- NO2
-data<- data[order(data[,8],data[,7]),]
-data<- as.matrix(data[,-c(8)])
-X<- as.matrix(data[,-c(1)])
-X<-data.frame(X)
-XNO2<-as.matrix(cbind(rep(1,500),X))
-YNO2<- as.matrix(data[,1])
-print(CR::benchmarks(XNO2,YNO2)$performance)
-print(CR::benchmarks(XNO2,YNO2)$quantiles)
-
-aarNO2<-CR::AAR(XNO2,YNO2,1)
-orrNO2<-CR::ORR(XNO2,YNO2,1)
-oslogNO2<-CR::OSLOG(XNO2,YNO2,1)
-coirrNO2<-CR::COIRR(XNO2,YNO2,1)
-
-perfNO2<-as.matrix(rbind(aarNO2$performance,orrNO2$performance,oslogNO2$performance,coirrNO2$performance))
-perfNO2<-cbind(perfNO2[,1],(perfNO2[,2]))
-colnames(perfNO2)<-c("RMSE","MAE")
-rownames(perfNO2)<-c("aar","orr","oslog","coirr")
-print(perfNO2)
-
-quantNO2<-as.matrix(rbind(aarNO2$quantiles,orrNO2$quantiles,oslogNO2$quantiles,coirrNO2$quantiles))
-colnames(quantNO2)<-c("25%","50%","75%")
-rownames(quantNO2)<-c("aar","orr","oslog","coirr")
-print(quantNO2)
-#ISE
 data<- ISE
-XISE<- as.matrix(data[,-c(1,3)])
-YISE<- as.matrix(data[,3])
+sample <- 1:floor(.25*nrow(data))
+train <- data[sample, ]
+test  <- data[-sample, ]
+Xtrain <- as.matrix(train[,-c(1,3)])
+Ytrain <-as.matrix(train[,3])
+Xtest <- as.matrix(test[,-c(1,3)])
+Ytest <- as.matrix(test[,3])
 
-print(benchmarks(XISE,YISE)$performance)
-print(benchmarks(XISE,YISE)$quantiles)
-aarISE<-CR::AAR(XISE,YISE,0.001953125)
-orrISE<-CR::ORR(XISE,YISE,0.0001220703)
-oslogISE<-CR::OSLOG(XISE,YISE,0.0009765625)
-coirrISE<-CR::COIRR(XISE,YISE,1.525879e-05)
+#tuningCOIRR<-tuneCOIRR(0.00000001,0.00001,0.000000001,Xtrain,Ytrain)
+#tuningORR<-tuneORR(0.00000001,0.00001,0.000000001,Xtrain,Ytrain)
+#tuningOSLOG<-tuneOSLOG(0.00000001,0.00001,0.000000001,Xtrain,Ytrain)
+#tuningAAR<-tuneAAR(0.00000001,0.00001,0.000000001,Xtrain,Ytrain)
 
-perfISE<-as.matrix(rbind(aarISE$performance,orrISE$performance,oslogISE$performance,coirrISE$performance))
-perfISE<-cbind(perfISE[,1],(perfISE[,2]))
-colnames(perfISE)<-c("RMSE","MAE")
-rownames(perfISE)<-c("aar","orr","oslog","coirr")
+print(AAR(Xtrain,Ytrain,tuningAAR$optimum)$Weights)
+print(ORR(Xtrain,Ytrain,tuningORR$optimum)$Weights)
+print(OSLOG(Xtrain,Ytrain,tuningOSLOG$optimum)$Weights)
+print(COIRR(Xtrain,Ytrain,tuningORR$optimum)$Weights)
+
+
+perfISE<-rbind(t(CR::ORR(Xtest,Ytest,1e-05)$performance),t(CR::AAR(Xtest,Ytest,1e-05)$performance),t(CR::OSLOG(Xtest[,-c(5)],Ytest,1e-05)$performance),t(CR::COIRR(Xtest[,-c(5)],Ytest,1e-05)$performance))
+colnames(perfISE)<-c("RMSE","R2","MAE")
+rownames(perfISE)<-c("orr","aar","oslog","coirr")
 print(perfISE)
+print(benchmarks(Xtest,Ytest)$performance)
 
-quantISE<-as.matrix(rbind(aarISE$quantiles,orrISE$quantiles,oslogISE$quantiles,coirrISE$quantiles))
-colnames(quantISE)<-c("25%","50%","75%")
-rownames(quantISE)<-c("aar","orr","oslog","coirr")
-print(quantISE)
 
-print(CR::time(Xtemp,Ytemp,1))
-print(CR::time(XNO2,YNO2,1))
+quant<-rbind(t(CR::ORR(Xtest,Ytest,1e-05)$quantiles),t(CR::AAR(Xtest,Ytest,1e-05)$quantiles),t(CR::OSLOG(Xtest[,-c(5)],Ytest,1e-05)$quantiles),t(CR::COIRR(Xtest[,-c(5)],Ytest,1e-05)$quantiles))
+colnames(quant)<-c("25%","50%","75%")
+rownames(quant)<-c("orr","aar","oslog","coirr")
+print(quant)
+
+print(benchmarks(Xtest,Ytest)$quantiles)
+
+F16<- F16
+devtools::use_data(F16, F16)
+sample <- 1:floor(.25*nrow(F16))
+train <- as.matrix(F16[sample,])
+test  <- as.matrix(F16[-sample,])
+Xtrain<- as.matrix(train[,1:40])
+Ytrain<-as.matrix(train[,41])
+Xtest<- as.matrix(test[,1:40])
+Ytest<- as.matrix(test[,41])
+
+
+perf<-rbind(t(CR::ORR(Xtest[,-c(26,30,32,34)],as.matrix(Ytest),1e-05)$performance),t(CR::AAR(Xtest[,-c(26,30,32,34)],as.matrix(Ytest),1e-05)$performance),t(CR::OSLOG(Xtest[,c(1:10,39)],as.matrix(Ytest),1e-05)$performance),t(CR::COIRR(Xtest[,c(1:10,39)],as.matrix(Ytest),1e-05)$performance))
+colnames(perf)<-c("RMSE","R2","MAE")
+rownames(perf)<-c("orr","aar","oslog","coirr")
+print(perf)
+print(benchmarks(Xtest,Ytest)$performance)
+
+
+quant<-rbind(t(CR::ORR(Xtest[,-c(26,30,32,34)],as.matrix(Ytest),1e-05)$quantiles),t(CR::AAR(Xtest[,-c(26,30,32,34)],as.matrix(Ytest),1e-05)$quantiles),t(CR::OSLOG(Xtest[,c(1:10,39)],as.matrix(Ytest),1e-05)$quantiles),t(CR::COIRR(Xtest[,c(1:10,39)],as.matrix(Ytest),1e-05)$quantiles))
+colnames(quant)<-c("25%","50%","75%")
+rownames(quant)<-c("orr","aar","oslog","coirr")
+print(quant)
+
+print(benchmarks(Xtest,Ytest)$quantiles)
